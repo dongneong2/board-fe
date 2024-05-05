45b2e3685f7bb0753faf6ca2b9c0d6760b35de6b
dongneong2 <dongneong2@naver.com>
2024-05-06 05:28:22 +0900

    기존 파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Todolist from "./components/Todolist";
+import React from 'react';
+import './App.css';
+import Todolist from './components/Todolist';
 
 function App() {
   return (
